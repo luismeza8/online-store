1a0f381deffed65e61116a565f41edd493c1b659
luismeza8 <angel841bv@gmail.com>
2025-01-10 22:49:42 -0700

    range for priceFilter added to ui

--- src/components/PriceFilter.jsx
+++ src/components/PriceFilter.jsx
@@ -1,15 +1,38 @@
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { useEffect, useState } from 'react';
 
 export default function PriceFilter() {
+  const minValue = 0;
+  const maxValue = 100;
+
+  const [firstValue, setFirstValue] = useState(minValue);
+  const [secondValue, setSecondValue] = useState(maxValue);
+
+  useEffect(() => {
+    console.log("first value:", firstValue)
+    console.log("second value:", secondValue)
+  }, [firstValue, secondValue]);
+
   return (
     <>
+      <h2>Filtrar por precio</h2>
       <Slider 
         range
-        min={0}
-        max={100}
-        defaultValue={[5, 20]}
+        value={[firstValue, secondValue]}
+        min={minValue}
+        max={maxValue}
+        defaultValue={[minValue, maxValue]}
+        onChange={(values) => {
+          setFirstValue(values[0])
+          setSecondValue(values[1])
+        }}
       />
+      <div className="flex w-full justify-between">
+        <p>Desde: ${ firstValue }</p>
+        <p>Hasta: ${ secondValue }</p>
+      </div>
+      <button>Filtrar</button>
     </>
   )
 }
--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -4,9 +4,7 @@ export default function RightSidebar() {
   return (
     <>
       <div className="m-4 h-screen w-1/5 bg-white flex flex-col">
-        <h2>Filtrar por precio</h2>
-        <PriceFilter/>
-        <button>Filtrar</button>
+        <PriceFilter />
       </div>
     </>
   );
