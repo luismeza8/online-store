4ad19405bd323fdba5a5d33f269b0f033fa4afef
luismeza8 <angel841bv@gmail.com>
2025-01-13 14:20:37 -0700

    bug in grey screen while loading fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home'
 function App() {
   return (
     <>
-      <div className='flex flex-col bg-gray-100 w-full h-auto'>
+      <div className='flex flex-col bg-gray-100 w-full min-h-screen'>
         <Navbar />
         <div className="h-full flex justify-center">
           <Home />
