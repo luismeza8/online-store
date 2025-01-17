a0c3085cb928db24828438f75c29d02cab2110a8
luismeza8 <angel841bv@gmail.com>
2025-01-17 12:47:41 -0700

    shopping cart icon added to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import ItemDetails from './pages/ItemDetails'
 function App() {
   return (
     <>
-      <div className='flex flex-col bg-gray-100 w-full min-h-screen'>
-        <Navbar />
-        <div className="h-full flex justify-center">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element= { <Home /> } />
-              <Route path="/item/:itemId" element={ <ItemDetails /> }></Route>
-            </Routes>
-          </BrowserRouter>
+      <BrowserRouter>
+        <div className='flex flex-col bg-gray-100 w-full min-h-screen'>
+          <Navbar />
+          <div className="h-full flex justify-center">
+              <Routes>
+                <Route path="/" element= { <Home /> } />
+                <Route path="/item/:itemId" element={ <ItemDetails /> }></Route>
+              </Routes>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router";
+
 export default function Navbar() {
   return (
     <>
-      <div className="bg-white w-full h-[8vh]">
-        <ul className='w-full h-full flex justify-evenly items-center'>
-          <li className='font-bold'>Shop</li>
-          <li>Home</li>
-          <li>About</li>
-          <li>Stuff</li>
-          <li>Pascual</li>
-        </ul>
+      <div className="bg-white w-full h-[8vh] flex justify-center">
+        <div className='w-[70%] h-full flex justify-between items-center'>
+          <div className="flex w-1/2 gap-12">
+            <Link to="/">SHOP</Link>
+            <Link to="/">Home</Link>
+            <Link to="/">About</Link>
+          </div>
+          <div className="flex w-2/6 justify-end items-center gap-12">
+            <Link>instagram</Link>
+            <Link>tiktok</Link>
+            <Link className="hover:bg-gray-100 p-1 rounded-full">
+              <img className="" src="src/assets/shopping_cart.svg" alt="" />
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   )
