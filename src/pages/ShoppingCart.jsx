edd5ea90cfd1192a41e489a4ecd4f90af692e7e2
luismeza8 <angel841bv@gmail.com>
2025-01-18 14:52:41 -0700

    attempt to fetch all items in shopping cart with a for loop

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
+
 export default function ShoppingCart() {
+  const URL = "https://fakestoreapi.com/products";
+  const cart = JSON.parse(localStorage.getItem("cart"));
+
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchItem(itemId) {
+      try {
+        const response = await fetch(`${URL}/${itemId}`);
+        const json = await JSON.parse(response);
+        console.log(json)
+        return(json);
+      } catch (e) {
+        console.error(e, "Error fetching in shopping cart" + e.message);
+        return({})
+      }
+    }
+
+    async function fetchAllItems() {
+      for (let i = 0; i <= cart.length; i++) {
+        const item = await fetchItem(cart[i]);
+        console.log(item)
+      }
+    }
+
+    fetchAllItems()
+    
+  }, []);
+  
   return (
-    <>
-    </>
+    <div className="bg-red-400 flex justify-center w-full h-full">
+      <div className="w-[60%] h-full bg-blue-400">
+        {cart}
+      </div>
+    </div>
   )
 }
