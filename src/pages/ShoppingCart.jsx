dabf78abdf32bebf639f3a4f33fa4ede78cbd456
luismeza8 <angel841bv@gmail.com>
2025-01-18 15:16:08 -0700

    fetching all the items in the shopping cart done

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function ShoppingCart() {
   const URL = "https://fakestoreapi.com/products";
-  const cart = JSON.parse(localStorage.getItem("cart"));
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -11,30 +11,42 @@ export default function ShoppingCart() {
     async function fetchItem(itemId) {
       try {
         const response = await fetch(`${URL}/${itemId}`);
-        const json = await JSON.parse(response);
-        console.log(json)
-        return(json);
+        const json = await response.json();
+        return json;
       } catch (e) {
-        console.error(e, "Error fetching in shopping cart" + e.message);
-        return({})
+        console.error(e, `Error fetching the items in the shopping cart ${e.message}`);
+        return {};
       }
     }
 
     async function fetchAllItems() {
-      for (let i = 0; i <= cart.length; i++) {
-        const item = await fetchItem(cart[i]);
-        console.log(item)
+      try {
+        const promises = cart.map((itemId) => fetchItem(itemId));
+        const results = await Promise.all(promises);
+        setItems(results);
+      } catch (e) {
+        console.error(e, `Error fetching all the items ${e.message}`);
+      } finally {
+        setLoading(false);
       }
     }
 
-    fetchAllItems()
+    if (cart.length > 0) {
+      fetchAllItems();
+    }
     
-  }, []);
+  }, [cart]);
   
   return (
     <div className="bg-red-400 flex justify-center w-full h-full">
       <div className="w-[60%] h-full bg-blue-400">
-        {cart}
+        { loading ? (
+          <p>Loading...</p>
+        ) : (
+          items.map((item) => (
+            <h2>{ item.title }</h2>
+          ))
+        ) }
       </div>
     </div>
   )
