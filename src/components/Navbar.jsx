f4e864d85575e5b71f647b4f8dbcf976b15cdd82
luismeza8 <angel841bv@gmail.com>
2025-01-30 14:29:22 -0700

    liks in navbar and dependencies fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,13 @@ export default function Navbar() {
       <div className="bg-white w-full h-[8vh] flex justify-center">
         <div className='w-[70%] h-full flex justify-between items-center'>
           <div className="flex w-1/2 gap-12">
-            <Link to="/">SHOP</Link>
+            <Link to="/">ONLINE SHOP</Link>
             <Link to="/">Home</Link>
-            <Link to="/">About</Link>
+            <Link to="/about">About</Link>
           </div>
           <div className="flex w-2/6 justify-end items-center gap-12">
-            <Link>instagram</Link>
-            <Link>tiktok</Link>
+            <a target="_blank" href="https://x.com/luis_mz8">Twitter</a>
+            <a target="_blank" href="https://github.com/luismeza8/online-store">Github</a>
             <Link 
               className="flex hover:bg-gray-100 p-1 rounded-full"
               to="/cart"
