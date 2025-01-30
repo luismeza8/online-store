147f3241043acedd57e42c2defcb3355ec35d5a4
luismeza8 <angel841bv@gmail.com>
2025-01-30 16:36:05 -0700

    icons added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,22 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="bg-white w-full h-[8vh] flex justify-center">
+      <div className="bg-white w-full h-[8vh] flex justify-center shadow-sm">
         <div className='w-[70%] h-full flex justify-between items-center'>
-          <div className="flex w-1/2 gap-12">
-            <Link to="/">ONLINE SHOP</Link>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+          <div className="flex items-center w-1/2 gap-12">
+            <Link className="text-blue-700 text-2xl font-extrabold" to="/">ONLINE SHOP</Link>
+            <Link className="hover:underline" to="/">Home</Link>
+            <Link className="hover:underline" to="/about">About</Link>
           </div>
           <div className="flex w-2/6 justify-end items-center gap-12">
-            <a target="_blank" href="https://x.com/luis_mz8">Twitter</a>
-            <a target="_blank" href="https://github.com/luismeza8/online-store">Github</a>
+            <a className="hover:underline flex items-center gap-1" target="_blank" href="https://x.com/luis_mz8">
+              <img className="w-[25px]" src="/public/twitter_icon.svg" alt="" />
+              <p>Twitter</p>
+            </a>
+            <a className="hover:underline flex items-center gap-1" target="_blank" href="https://github.com/luismeza8/online-store">
+              <img src="/public/github_icon.svg" alt="" />
+              <p>Github</p>
+            </a>
             <Link 
               className="flex hover:bg-gray-100 p-1 rounded-full"
               to="/cart"
